4dbce4770c4d7366388bd4db3c42a30e318bc1c1
Răzvan Roșu <razvan.rosu89@gmail.com>
2018-07-26 11:06:12 +0300

    Remove build folder

--- build/page1.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],[function(s,o){"use strict";console.log("This is PAGE ONE")}]);
-//# sourceMappingURL=page1.bundle.js.map
--- build/page2.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],[function(s,o){"use strict";console.log("This is PAGE TWO")}]);
-//# sourceMappingURL=page2.bundle.js.map
